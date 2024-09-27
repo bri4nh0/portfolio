b8be8e414155042147e9e2b1406e912a0c2de4d0
brianho <89711814+bri4nh0@users.noreply.github.com>
2024-09-28 00:57:44 +0800

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,6 @@
+'user client'
+
+export default function myImageLoader({ src, width, quality }) {
+    if(src.startsWith('https://images.pexels.com')) return src
+    return 'https://brianportfolio.com/${src?w=${width}&q=${quality || 75}'
+} 
